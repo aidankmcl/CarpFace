090a078cde78c897460d6005c8ccbdd8e120614a
songbird175 <celina.bekins@students.olin.edu>
2017-02-02 10:25:27 -0500

    Initialized purpose script

---
+++ email_purpose.py
@@ -0,0 +1,4 @@
+"""This script takes in parsed email data and analyzes it, looking for
+specific terms which might identify what an email is about and organizing
+emails based on the use of those terms."""
+
