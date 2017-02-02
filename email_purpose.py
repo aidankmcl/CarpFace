4ff0b6f965b058cbf5badeddd58404b2d4a15fa8
songbird175 <celina.bekins@students.olin.edu>
2017-02-02 10:33:47 -0500

    Purpose script successfully loads json file data

--- email_purpose.py
+++ email_purpose.py
@@ -2,3 +2,10 @@
 specific terms which might identify what an email is about and organizing
 emails based on the use of those terms."""
 
+import json
+from pprint import pprint
+
+with open('parsed_data/january-2017.json') as data_file:    
+    data = json.load(data_file)
+
+pprint(data)
